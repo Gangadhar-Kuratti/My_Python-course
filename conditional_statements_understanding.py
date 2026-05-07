b0762b189f795955c6e6665423f7f208994d0cce
Gangadhar <gangadharkuratti790@gmail.com>
2026-05-07 16:58:00 +0530

    added understanding programs

---
+++ conditional_statements_understanding.py
@@ -0,0 +1,47 @@
+# program to understand the if-else
+
+x = 10
+
+if x > 5:
+    print("A")
+else:
+    print("B")
+
+# program to understand the if-elif-else
+
+x = 3
+
+if x > 5:
+    print("A")
+elif x > 2:
+    print("B")
+else:
+    print("C")
+
+# program to understand the if-if condition
+
+x = 8
+
+if x > 5:
+    print("A")
+if x > 7:
+    print("B")
+
+# program to understand the > operator
+
+age = 18
+
+if age > 18:
+    print("Adult")
+else:
+    print("Teen")
+
+# program to understand nestef if condition
+
+x = 15
+
+if x > 10:
+    if x > 20:
+        print("A")
+    else:
+        print("B")        
