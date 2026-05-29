713b83485770edee1a878828103900c08d45d081
Gangadhar <gangadharkuratti790@gmail.com>
2026-05-29 11:29:05 +0530

    added programs

---
+++ for_loop_practice_programs/for_loop_practice_programs5.py
@@ -0,0 +1,10 @@
+# program for list comprehension using for loop
+l=[1,2,3,4,5]
+dl=[i*2 for i in l]
+print(dl)
+
+# program for dictionary comprehension usong for loop
+
+d={"nikhil","nikhi","nikh","nik","ni","n"}
+dd={i:len(i) for i in d}
+print(dd)
