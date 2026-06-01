f817455248b3f0c82e7c706817753ff612b2f9a0
Gangadhar <gangadharkuratti790@gmail.com>
2026-06-01 22:35:47 +0530

    Organized Python files

--- for_loop_practice_problems2.py
+++
@@ -1,7 +0,0 @@
-# program to print the combined list of dictionaries   
-name=["naveen","kiran"]
-marks=[90,80]
-name_marks={}
-for index,i in enumerate(name):
-    name_marks[i]=marks[index]
-print(name_marks)
--- for_loop_practice_problems3.py
+++
@@ -1,5 +0,0 @@
-# program to understand the dictionary compreesion
-
-d={"gangadhar","kiran","kavya"}
-d1={name:len(name) for name in d}
-print(d1)
