534001a1e41016b626aa68ae55f7f7fb298dd904
Gangadhar <gangadharkuratti790@gmail.com>
2026-06-05 20:39:52 +0530

    added programs

---
+++ Functions/practice_programs2.py
@@ -0,0 +1,28 @@
+# program to print the numbers  from 1-5
+def number():
+    for i in range(1,6):
+        print(i)
+number()
+
+# program to print the square of a number
+def square(num):
+    print(num**2)
+square(5)
+
+# program to print the sum of two numbers
+def summation (num1,num2):
+    print(num1+num2)
+summation(2,3)
+
+# program to print the length of a string 
+def ex(string):
+    print(len(string))
+ex("nikhil")
+
+# program to print the sum of numbers using return type
+def summation(num1,num2):
+    return int(num1+num2)
+a = summation(2,3)
+print(a)
+
+    
