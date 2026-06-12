28a31dae7e01e630a707941edc1fff39b23948d2
Gangadhar <gangadharkuratti790@gmail.com>
2026-06-12 18:43:13 +0530

    added program

---
+++ Exam_practice_programs/exam_practice_program1.py
@@ -0,0 +1,19 @@
+# Program for ATM Withdrawal Operation Using Exception Handling
+
+try :
+    balance=10000
+    
+    withdrawal_amount=int(input("Enter amount: "))
+    
+    if withdrawal_amount>balance:
+        raise Exception("Insufficient balance")
+    balance = balance - withdrawal_amount
+    
+    print("Withdrawan successfully")
+    print("Remaining Balance is: ",balance)
+    
+except ValueError:
+    print("Enter numbers only")
+    
+except Exception as e:
+    print(e)
