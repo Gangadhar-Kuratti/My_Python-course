1ef3cc00cdc696506258a1ee6e0342f3ce15e9c8
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-02 22:59:20 +0530

    Added program

---
+++ OOP_Concepts/practice_pro.py
@@ -0,0 +1,14 @@
+# Class and Object Example
+
+class Student:
+
+    def display(self):
+        print("Welcome to Python OOP!")
+
+# Creating objects
+student1 = Student()
+student2 = Student()
+
+# Calling method
+student1.display()
+student2.display()
