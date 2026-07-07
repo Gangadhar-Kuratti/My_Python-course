e1f7af18f88bd1fbcd39a6e627f2aedca7a21c5a
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-07 21:19:04 +0530

    added program

---
+++ OOP_Concepts/practice_program.py
@@ -0,0 +1,18 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+
+s = Student("chandu", 19)
+
+s.set_name("darshan")
+
+print(s.get_name())
