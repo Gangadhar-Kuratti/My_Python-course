ac3235e83dd8a6e5399a8a9693cb60e92476ec45
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-13 21:42:10 +0530

    added program

---
+++ OOP_Concepts/practice_program3.py
@@ -0,0 +1,13 @@
+# Class and Object Example
+
+class Student:
+
+    def display(self):
+        print("This is an example for object creation and method caller")
+
+# Creating objects
+student1 = Student()
+
+
+# Calling method
+student1.display()
